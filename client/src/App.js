f3598ca31a090aac05ce1c8a93b31fb3d9664419
len124 <vnyakundi@student.kibu.ac.ke>
2023-02-21 02:24:20 +0300

    commit

---
+++ client/src/App.js
@@ -0,0 +1,46 @@
+import { createBrowserRouter, Outlet, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Product from "./pages/Product/Product";
+import Navbar from "./components/Navbar/Navbar";
+import Footer from "./components/Footer/Footer";
+import Products from "./pages/Products/Products";
+
+const Layout = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "/product/:id",
+        element: <Product />,
+      },
+      {
+        path: "/products/:id",
+        element: <Products />,
+      },
+    ],
+  },
+]);
+function App() {
+  return (
+    <div>
+      <RouterProvider router={router} />
+    </div>
+  );
+}
+
+export default App;
