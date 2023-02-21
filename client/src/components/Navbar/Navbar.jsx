04091d6ae4e6a7cbb94b513ee271c035376b0345
len124 <vnyakundi@student.kibu.ac.ke>
2023-02-22 00:20:16 +0300

    commit

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
-import './Navbar.scss'
+import React from "react";
+import "./Navbar.scss";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import SearchIcon from "@mui/icons-material/Search";
+import FavoriteBorderIcon from "@mui/icons-material/FavoriteBorder";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
-  )
-}
+    <div className="navbar">
+      <div className="wrapper">
+        <div className="left">
+          <div className="item">
+            <img src="images/ken.jpg" alt="" />
+            < KeyboardArrowDownIcon/>
+          </div>
+          <div className="item">
+            <span>KSH</span>
+            < KeyboardArrowDownIcon/>
+          </div>
+        </div>
+        <div className="center"></div>
+        <div className="right"></div>
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
